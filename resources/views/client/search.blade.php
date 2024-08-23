238d5a5d7b65705172cf932d8ab3fc1a81e1145b
basyair7 <basyair.fathul@gmail.com>
2024-08-23 15:27:41 +0700

    add search page

--- resources/views/client/search.blade.php
+++ resources/views/client/search.blade.php
@@ -85,5 +85,10 @@
     </div>
 
     @include('includes.home.footer')
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    
 </body>
 </html>
